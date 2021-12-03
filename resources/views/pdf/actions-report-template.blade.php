1f27a72e893cdc06096928865a5cea294f6c628c
Mohamed Gaber <mohamed.gaberiti40@gmail.com>
2021-12-03 21:59:27 +0200

    style actions  report template

--- resources/views/pdf/actions-report-template.blade.php
+++ resources/views/pdf/actions-report-template.blade.php
@@ -1,18 +1,53 @@
-<table>
-    <thead>
-    <tr>
-        <th >User</th>
-        <th>Tweets count</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($users as $user)
-        <tr>
-            <th>{{$user->name}}</th>
-            <th>{{$user->tweets_count}}</th>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+<style>
+    *, *:before, *:after {
+        box-sizing: border-box;
+    }
+    table{
+        background: #ddd;
+        width: 100%;
+    }
 
-<p>tweets avg per user : {{$tweetsPerUserAvg}}</p>
+    table thead{
+        background: black;
+        height: 2em;
+    }
+
+    table tbody td{
+        text-align: center;
+        height: 2em;
+        border-bottom: 1px solid;
+        border-left: 1px solid;
+        font-weight: bold;
+    }
+
+    table thead th{
+       color: #fff;
+    }
+    .avg{
+        font-weight: bold;
+    }
+
+</style>
+<div class="container ">
+    <table >
+        <thead>
+        <tr>
+            <th>User</th>
+            <th>Tweets count</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+            <tr>
+                <td>{{$user->name}}</td>
+                <td>{{$user->tweets_count}}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    <div class="container">
+        <p class="avg">Tweets average <span class="avg-number">{{$tweetsPerUserAvg}}</span> per user</p>
+
+    </div>
+</div>
+
