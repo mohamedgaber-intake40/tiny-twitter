91a8119ab4dc84247bc7933cd77c6859a174a414
Mohamed Gaber <mohamed.gaberiti40@gmail.com>
2021-12-03 22:21:02 +0200

    enhance response with ResponseHelpers trait

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
-use App\Http\Resources\SuccessResource;
 use App\Http\Resources\TokenResource;
 use App\Services\Auth\LoginService;
 use App\Services\Auth\LogoutService;
 use App\Services\Auth\RegisterService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
@@ -21,45 +22,40 @@ class AuthController extends Controller
     /**
      * @param RegisterRequest $request
      * @param RegisterService $registerService
-     * @return JsonResponse|object
+     * @return Application|ResponseFactory|\Illuminate\Http\Response
      */
     public function register(RegisterRequest $request, RegisterService $registerService)
     {
         ['user' => $user, 'token' => $token] = $registerService->handle($request->validated() + ['token_name' => \request()->userAgent()]);
-
-        return SuccessResource::make([
+        return $this->successResponse([
             'user'  => [
                 'id'    => $user->id,
                 'image' => $user->image_url
             ],
-            'token' => TokenResource::make($token),
-        ])->additional(['message' => __('success.auth.register')])
-          ->response()
-          ->setStatusCode(Response::HTTP_CREATED);
+            'token' => TokenResource::make($token)
+        ], __('success.auth.register'), Response::HTTP_CREATED);
     }
 
     /**
      * @param LoginRequest $request
      * @param LoginService $loginService
-     * @return TokenResource
+     * @return Application|\Illuminate\Http\Response|ResponseFactory
      * @throws ValidationException
      */
     public function login(LoginRequest $request, LoginService $loginService)
     {
         $token = $loginService->handle($request->validated() + ['token_name' => \request()->userAgent()]);
-        return TokenResource::make($token)
-                            ->additional(['message' => __('success.auth.login')]);
+        return $this->successResponse(TokenResource::make($token), __('success.auth.login'));
     }
 
     /**
      * @param LogoutService $logoutService
-     * @return SuccessResource
+     * @return Application|ResponseFactory|\Illuminate\Http\Response
      */
     public function logout(LogoutService $logoutService)
     {
         $logoutService->handle();
-        return SuccessResource::make([])
-                              ->additional(['message' => __('success.auth.logout')]);
+        return $this->successResponse([], __('success.auth.logout'));
     }
 
 }
